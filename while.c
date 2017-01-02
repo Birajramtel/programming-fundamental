5406ad2a15671e0629f32fd41ecc7eb74453e5e5
Birajramtel <Biraj.ramtel007@gmail.com>
2017-01-02 10:29:49 +0545

    Add files via upload

---
+++ while.c
@@ -0,0 +1,26 @@
+/*
+Student Name:Biraj ramtel
+Subject:computer
+Roll No:11
+Program :while loop
+Lab No:02
+Date:02/01/2017
+*/
+
+
+
+#include<stdio.h>
+int main()
+{
+int a;
+printf("Enter the number");
+scanf("%d",&a);
+while(a%2==0)
+{
+printf("The number is even\n");
+}
+return
+printf("The number is odd\n");
+return 0;
+}
+
