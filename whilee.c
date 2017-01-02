9b3ac8b1c5f2799dcb9041cd9c3db50665bc4493
Birajramtel <Biraj.ramtel007@gmail.com>
2017-01-02 10:29:25 +0545

    Add files via upload

---
+++ whilee.c
@@ -0,0 +1,18 @@
+/*
+Name:Biraj Ramtel
+Roll num:11
+*/
+
+
+#include<stdio.h>
+int main()
+{
+int a=1,n;
+printf("Enter the number");
+scanf("%d",&n);
+while(a<=n)
+{
+printf("%d",a++);
+}
+return 0;
+}
