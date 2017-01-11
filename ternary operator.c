69973469bb836ce238c6f7bf22f39aa6df843289
Birajramtel <Biraj.ramtel007@gmail.com>
2017-01-11 10:58:48 +0545

    Add files via upload

---
+++ ternary operator.c
@@ -0,0 +1,16 @@
+/*
+Name:-Biraj Ramtel
+Roll no:-11
+Subject:-Programme Fundamental
+lab:-16
+programme:- WAP to check the leap year
+*/
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the year");
+scanf("%d",&n);
+(n%4==0 &&a%100!=0)?printf("the year is leap year"):(a%400==0)?printf("the year is leap year"):printf("the year is not leap year");
+return 0;
+}
