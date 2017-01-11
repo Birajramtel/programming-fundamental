631dd3d2f434811bd781984fcbe3432e9681b4b8
Birajramtel <Biraj.ramtel007@gmail.com>
2017-01-11 10:59:49 +0545

    Add files via upload

---
+++ first n last.c
@@ -0,0 +1,19 @@
+/*
+Name: Biraj Ramtel
+Roll:
+Subject : Programming fundamental
+Lab sheet: 17
+Program : WAP  to  
+Date : 11jan 2017
+*/
+#include<stdio.h>
+int main()
+{
+int n,res,a,res2;
+printf("Enter the four digit number");
+scanf("%d",&n);
+res=n/10;
+printf("the result is: %d\n",res);
+
+return 0;
+}
