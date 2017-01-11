da6ab986aa78be320984daa784456ae78a25bb4d
Birajramtel <Biraj.ramtel007@gmail.com>
2017-01-11 10:58:10 +0545

    Add files via upload

---
+++ first.c
@@ -0,0 +1,20 @@
+/*
+Name: Biraj Ramtel
+Roll:
+Subject : Programming fundamental
+Lab sheet: 17
+Program : WAP  to  
+Date : 11jan 2017
+*/
+#include<stdio.h>
+int main()
+{
+int n,a,res3;
+printf("Enter the number");
+scanf("%d",&n);
+printf("Enter the number");
+scanf("%d",&a);
+res3=n%a;
+printf("The remainder is %d",res3);
+return 0;
+}
