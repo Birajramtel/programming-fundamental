f7f1eac7aa1d25aa050a40e7de8ab7bff1b70347
Birajramtel <Biraj.ramtel007@gmail.com>
2017-01-11 10:59:03 +0545

    Add files via upload

---
+++ while loop.c
@@ -0,0 +1,21 @@
+/*
+Name:-Biraj Ramtel
+Roll no:-11
+Subject:-Programme Fundamental
+lab:-16
+programme:- WAP to print all natural numbers
+*/
+#include<stdio.h>
+int main()
+{
+int n=0;
+printf("Enter the number");
+scanf("%d",&n
+);
+while(n<=100)
+{
+printf("%d\n",n);
+n++;
+}
+return 0;
+}
