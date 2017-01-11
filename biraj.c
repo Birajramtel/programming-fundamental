c939c05c54af4423636b57adc5d5797468eedab5
Birajramtel <Biraj.ramtel007@gmail.com>
2017-01-11 10:57:55 +0545

    Add files via upload

---
+++ biraj.c
@@ -0,0 +1,16 @@
+/*
+Name:-Biraj Ramtel
+Roll no:-11
+Subject:-Programming fundamental
+Program:-WAP to check the number is odd or even using ternary operator 
+lab:-16
+*/
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter the number");
+	scanf("%d",&n);
+	(n%2==0)? printf("the number is even"): printf("the number is odd");
+	return 0;
+}
