5857641ea25eea759f6630efd30afaa7447351f4
Birajramtel <Biraj.ramtel007@gmail.com>
2017-02-06 10:23:55 +0545

    Add files via upload

---
+++ b.c
@@ -0,0 +1,59 @@
+/*
+name:Biraj Ramtel
+subject:programming fundamental
+programme: wap to create a structure,assign value to it and display it
+roll no:11
+lab sheet no:26
+date:3 febuary 2017
+*/
+#include<stdio.h>
+struct student
+{
+int student_id;
+int registration_id;
+float fee;
+};
+int main()
+{
+struct student sambhav;
+sambhav.student_id=101;
+sambhav.registration_id=1005;
+sambhav.fee=10000.50;
+printf("sambhav id =%d\n",sambhav.student_id);
+printf("sambhav registration id =%d\n",sambhav.registration_id);
+printf("sambhav fee=%f\n",sambhav.fee);
+struct student suraj;
+suraj.student_id=102;
+suraj.registration_id=1006;
+suraj.fee=100000.50;
+printf("suraj id =%d\n",suraj.student_id);
+printf("suraj registration id =%d\n",suraj.registration_id);
+printf("suraj fee=%f\n",suraj.fee);
+return 0;/*
+name:Biraj Ramtel
+subject:programming fundamental
+programme: wap to create a structure,assign value to it and display it
+roll no:11
+lab sheet no:26
+date:3 febuary 2017
+*/
+#include<stdio.h>
+struct student
+{
+int student_id;
+int registration_id;
+float fee;
+};
+int main()
+{
+struct student sambhav;
+sambhav.student_id=101;
+sambhav.registration_id=1005;
+sambhav.fee=10000.50;
+printf("sambhav id =%d\n",sambhav.student_id);
+printf("sambhav registration id =%d\n",sambhav.registration_id);
+printf("sambhav fee=%f\n",sambhav.fee);
+return 0;
+}
+s
+}
