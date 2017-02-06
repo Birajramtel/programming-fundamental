79642b8f5663bfa34d7114636214c0d30bab584f
Birajramtel <Biraj.ramtel007@gmail.com>
2017-02-06 10:26:21 +0545

    Add files via upload

---
+++ bi1.c
@@ -0,0 +1,21 @@
+/*
+Student Name : Biraj Ramtel
+Subject : Programming Fundamentals
+Roll No : 11
+Program : WAP to display the following output: 
+			
+			       1
+			       2 2
+			       3 3 3	
+			       4 4 4 4
+			       5 5 5 5 5
+	
+Lab Sheet No : 19
+Date : 16 Jan 2017
+*/
+#include<stdio.h>
+int main ()
+{
+	int i,j,n;
+	printf("Enter the number:");
+	scanf("%d",&n);
