ae478ec9c3147462267855ef7042479a3014bc01
Birajramtel <Biraj.ramtel007@gmail.com>
2017-02-06 10:47:57 +0545

    Add files via upload

---
+++ fun.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void function (int num);
+int main()
+{
+void function1(int num)
+{
+printf("The no is %d ",num);
+}
+int num;
+printf("Enter the number :");
+scanf("%d",&num);
+function1(num);
+return 0;
+}
