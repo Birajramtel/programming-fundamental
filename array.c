928cf694f808ef4b69ea9a1d85b7fdcfde287157
Birajramtel <Biraj.ramtel007@gmail.com>
2017-02-06 10:19:07 +0545

    Add files via upload

---
+++ array.c
@@ -0,0 +1,19 @@
+/*
+Student Name : Biraj Ramtel
+Subject : Programming Fundamentals
+Roll No : 11
+Program : Write a C program to  write and arrey
+Lab Sheet No : 23
+Date : 01 feb 2017
+*/
+#include<stdio.h>
+int main()
+{
+	int a[5]={1,2,3,4,5};
+	int i;
+	for(i=0;i<5;i++)
+	{
+		printf("a[%d]=%d\n",i,a[i]);
+		}
+	return 0;
+	}
