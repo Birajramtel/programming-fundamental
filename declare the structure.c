739d63613684369f40652ce4d748880a3bbc23c8
Birajramtel <Biraj.ramtel007@gmail.com>
2017-02-06 10:43:01 +0545

    Add files via upload

---
+++ declare the structure.c
@@ -0,0 +1,9 @@
+/*
+name:Biraj Ramtel
+subject:programming fundamental
+programme: WAP to create a structure with two integer number and call the structure
+in the main method and add both integer value and display the result. 
+roll no:11
+lab sheet no:27
+date:06 febuary 2017
+*/
