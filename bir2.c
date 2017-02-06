48f584c210cc43ddebf0866a24df82570a3529b2
Birajramtel <Biraj.ramtel007@gmail.com>
2017-02-06 10:26:36 +0545

    Add files via upload

---
+++ bir2.c
@@ -0,0 +1,25 @@
+/*
+Student Name : Biraj Ramtel
+Subject : Programming Fundamentals
+Roll No : 11
+Program : WAP to display the following output: 
+			
+			      1 2 3
+			      1 2 3
+Lab Sheet No : 19
+Date : 16 Jan 2017
+*/
+#include<stdio.h>
+int main ()
+{
+	int i,j;
+	for(i=1;i<=2;i++)
+{
+	for(j=1;j<=3;j++)
+	{
+		printf("%d",j);
+		}
+		printf("\n");
+	}
+return 0;
+}
