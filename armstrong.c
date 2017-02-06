0615cd7dfbbbce879a5ead918dc39a28a92c44a8
Birajramtel <Biraj.ramtel007@gmail.com>
2017-02-06 10:18:34 +0545

    Add files via upload

---
+++ armstrong.c
@@ -0,0 +1,32 @@
+/*
+Student Name : Biraj Ramtel
+Subject : Programming Fundamentals
+Roll No : 11
+Program : Write a C program to fibonacci number
+Lab Sheet No : 19
+Date : 18 Jan 2017
+*/
+#include<stdio.h>
+int main()
+{
+int n,rev=0,rem,c;
+printf("Enter any number");
+scanf("%d",&n);
+c=n;
+while(n>0)
+{
+rem=n%10;
+a=rem*rem*rem;
+are
+n=n/10;
+}
+
+if(rev==c)
+{
+	printf("The number is pallidram");
+	}
+	else
+	{
+		printf("The number is not pallidram\n");
+		}
+}
